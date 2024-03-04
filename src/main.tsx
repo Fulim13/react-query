018fea3ce25dbd7cbca303b93d144d4ed72ea899
Fu Lim <fulim1130@gmail.com>
2024-03-04 18:50:17 +0800

    npm i @tanstack/react-query-devtools@4.28

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
@@ -11,6 +12,14 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* only for development,  not gonna include in production build*/}
+      {/* Observer is how many component is using the react query (like todos key),
+      when unmount, it will be removed from the observer list and obsever number will be decreased
+       */}
+      {/* 
+       default cache time is 5 minutes, so if the component use that react query key have been unmounted, the cache (todos) will be removed after 5 minutes
+       */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
