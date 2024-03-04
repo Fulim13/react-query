1e63fff43de4be3a03d2999d37b4a7fb7aff8fd7
Fu Lim <fulim1130@gmail.com>
2024-03-04 18:34:40 +0800

    Creating a Custom Query Hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,33 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "../hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    // at runtime, the queryFn function is called and the promise is resolved,we get an array of Todo[], the data is stored in the cache with the queryKey as the cache key
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
-  // 1. Auto Retries if the request fails
-  // 2. Auto Refetching at a specified interval(can be customized)
-  // 3. Caching : First time the data is fetched, it is stored in the cache, so if we navigate to another page and come back, the data is retrieved from the cache
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    // unique identifier for the query, used as the cache key, so anytime we retrieve a piece of data from the backend, the data is stored in the cache with this key
-    queryKey: ["todos"],
-    // function that returns the promise of the data we want to fetch
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
   if (isLoading) return <p>Loading...</p>;
 
