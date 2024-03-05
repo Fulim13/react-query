52c43f5dbcd4fb72622fd4c2c21b41b53b8205f1
Fu Lim <fulim1130@gmail.com>
2024-03-05 16:13:11 +0800

    Mutating Data

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,48 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "../hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      console.log(savedTodo);
+      // APPROACH: 1. Invalidate the cache
+      // we can tell react query that what in our cache is invalid, and it should refetch the all the data from the server, However it is not work in jsonplaceholder because the object we send to the server is not gonna save in the server, so we can't refetch the data from the server
+
+      // invalidate all the query with the key "todos"
+      // queryClient.invalidateQueries({
+      //   queryKey: ["todos"],
+      // });
+
+      // APPROACH: 2. Update the data in the cache
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current && ref.current.value)
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            completed: false,
+            userId: 1,
+          });
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
